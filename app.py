762a080bcdca55e3ba4d2920dcba6bb08bd8c99f
rahulkate173 <rahulkate173@gmail.com>
2025-11-24 09:47:11 +0530

    Final changes in server file of frontend and tfserving

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import render_template,redirect,url_for,Flask,request
 import uvicorn
+import requests
 import os
 
 app = Flask(__name__)
 UPLOAD_FOLDER = "static/uploads"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+FAST_API_URL = r"http://localhost:8000/predict"
 
 @app.route('/')
 def index():
@@ -16,8 +18,11 @@ def predict():
     filepath = os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
     file.save(filepath) 
     ### tensorflow pipeline 
-    confidence = 99.99
-    pred_class = 'Healthy'
+    with open(filepath, "rb") as f:
+        files = {"file": (file.filename, f.read(), file.mimetype)}
+    dict_pred = requests.post(FAST_API_URL,files=files).json()
+    confidence = dict_pred['confidence']
+    pred_class = dict_pred['class_name']
     return render_template(
         "result.html",
         image_url=url_for('static',filename=f"uploads/{file.filename}"),
@@ -27,3 +32,8 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True,port=8080,host='localhost')
+
+
+### what i had in pipeline 
+### app -> localhost:8080
+### pipeline -> rest_api_port: 8051
